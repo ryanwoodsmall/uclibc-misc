aaee520dad9b2952dc6d77e396b051be958ad781
ryan woodsmall <rwoodsmall@gmail.com>
2016-10-20 05:11:41 +0000

    dumb script to install gcc wrapper

---
+++ bin/install-gcc-wrapper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+scriptdir="$( cd "$(dirname "${BASH_SOURCE[0]}")/.." ; pwd)"
+
+for d in bin lib ; do
+	test -e /usr/local/uclibc-ng/${d} || {
+		mkdir -p /usr/local/uclibc-ng/${d}
+	}
+done
+
+install --mode=0755 ${scriptdir}/gcc-wrapper/uclibc-ng-gcc /usr/local/uclibc-ng/bin/
+install --mode=0644 ${scriptdir}/gcc-wrapper/uclibc-ng-gcc.specs /usr/local/uclibc-ng/lib/
